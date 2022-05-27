9b476e4c1157b45901d19cc171a8ad625ca1a3c6
diyabansal1 <93111003+diyabansal1@users.noreply.github.com>
2022-05-27 22:26:21 +0530

    Add files via upload

---
+++ Numpy/numpy ques5.py
@@ -0,0 +1,7 @@
+# Write a NumPy program to Get the positions where elements of a and b match
+
+import numpy as np
+a = np.array([1,2,3,2,3,4,3,4,5,6])
+b = np.array([7,2,10,2,7,4,9,4,9,8])
+
+np.where(a == b)
