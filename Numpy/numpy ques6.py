201524244205daae92806fdb0a64ab64903f6852
diyabansal1 <93111003+diyabansal1@users.noreply.github.com>
2022-05-27 22:26:38 +0530

    Add files via upload

---
+++ Numpy/numpy ques6.py
@@ -0,0 +1,9 @@
+# Convert all the elements of a numpy array from float to integer datatype
+
+import numpy as np
+a = np.array([[2.5, 3.8, 1.5],
+              [4.7, 2.9, 1.56]])
+
+o = a.astype('int')
+
+print(o)
