e1eb103a0389aa4eab198faa5df24a7cebe391a4
diyabansal1 <93111003+diyabansal1@users.noreply.github.com>
2022-05-27 22:33:26 +0530

    Add files via upload

---
+++ Functions/func ques3.py
@@ -0,0 +1,15 @@
+# Write a Python function that takes a list and returns a new list with unique elements of the first list.
+
+def f(l):
+    
+    x = []
+
+    for a in l:
+
+        if a not in x:
+
+            x.append(a)
+
+    return x
+
+print(f([1,2,3,3,3,3,4,5]))
