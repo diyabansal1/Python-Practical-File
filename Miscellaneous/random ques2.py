c75704c7b1a8372d21841327f807ae95932374da
diyabansal1 <93111003+diyabansal1@users.noreply.github.com>
2022-05-27 22:37:06 +0530

    Add files via upload

---
+++ Miscellaneous/random ques2.py
@@ -0,0 +1,8 @@
+# In a school fest, three randomly chosen students out of 100 students(having roll no 1-100) have to present bouquets to the guests. Help the school authorities chosen three students randomly.
+
+import random
+student1=random.randint(1,100)
+student2=random.randint(1,100)
+student3=random.randint(1,100)
+print("3 chosen students are:",)
+print(student1,student2,student3)
